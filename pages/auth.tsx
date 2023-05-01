534ecafdc00d5d1626e2c0a54be31ba79cf1340d
Fred Denis <fredjamo68@gmail.com>
2023-05-01 05:35:03 +0300

    Auth system

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,12 @@
 import Input from "@/components/Input";
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 import { useCallback, useState } from "react";
 
 const Auth = () => {
+  const router = useRouter();
+
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -10,9 +15,39 @@ const Auth = () => {
 
   const toggleVariant = useCallback(() => {
     setVariant(currentVariant =>
-      currentVariant == "login" ? "register" : "login"
+      currentVariant === "login" ? "register" : "login"
     );
   }, []);
+
+  // Login
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  // Register to app
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login()]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -51,7 +86,10 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
               {variant == "login" ? "login" : "Sign up"}
             </button>
             <p className="text-neutral-500 mt-12">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <h2 className="text-2xl text-green-500 ">Tujuna</h2>
 
-      
+       
     </>
   );
 }
