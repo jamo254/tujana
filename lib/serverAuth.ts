a69f65293814c00fc7873ca6c954fba55d7f81d3
Fred Denis <fredjamo68@gmail.com>
2023-05-02 15:48:22 +0300

    checking user login session

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -2,7 +2,26 @@ import { NextApiRequest } from 'next';
 import { getSession } from 'next-auth/react';
 
 import prismadb from "@/lib/prismadb";
+import { now } from 'next-auth/client/_utils';
 
 const serverAuth = async (req: NextApiRequest) => {
-    
-}
+    // Getting login user
+    const session = await getSession({ req });
+
+    if (!session?.user?.email) {
+        throw new Error("Not Signed in");
+        
+    }
+    const currentUser = await prismadb.user.findUnique({
+        where: {
+            email: session.user.email
+        }
+    });
+
+    if (!currentUser) {
+        throw new Error('Not Signed in');
+    }
+    return { currentUser }
+};
+
+export default serverAuth;
